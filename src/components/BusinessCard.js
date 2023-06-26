8c1d312b0d6b6245c54f1b942b5287531f87ad5c
mourad <mourad.latoui.89@gmail.com>
2023-06-26 17:00:43 -0400

    module 3 challenge 1 and 2

---
+++ src/components/BusinessCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class BusinessCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isVisible: true,
+    };
+  }
+
+  render() {
+    const { name, email, tel, photo } = this.props;
+    const { isVisible } = this.state;
+
+    return (
+      isVisible && (
+        <div className="business-card">
+          <div className="image-container">
+            <img src={photo} alt="Profile" />
+          </div>
+          <div className="info-container">
+            <h2>{name}</h2>
+            <p>Email: {email}</p>
+            <p>Tel: {tel}</p>
+            <button onClick={() => this.setState({ isVisible: false })}>
+              Remove
+            </button>
+          </div>
+        </div>
+      )
+    );
+  }
+}
+
+export default BusinessCard;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,48 @@
 import { Hello } from './hello';
+import BusinessCard from './BusinessCard';
 
 export const Main = (props) => {
   return (
     <main class="main">
       <Hello name={props.name} />
+      <div className="business-cards-container">
+        <BusinessCard
+          name="Person One"
+          email="person.one@mcgill.com"
+          tel="123-456-7890"
+          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
+        />
+        <BusinessCard
+          name="Person Two"
+          email="person.two@mcgill.com"
+          tel="123-456-7890"
+          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
+        />
+        <BusinessCard
+          name="Person Three"
+          email="person.three@mcgill.com"
+          tel="123-456-7890"
+          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
+        />
+        <BusinessCard
+          name="Person Four"
+          email="person.four@mcgill.com"
+          tel="123-456-7890"
+          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
+        />
+        <BusinessCard
+          name="Person Five"
+          email="person.five@mcgill.com"
+          tel="123-456-7890"
+          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
+        />
+        <BusinessCard
+          name="Person Six"
+          email="person.six@mcgill.com"
+          tel="123-456-7890"
+          photo="https://banffventureforum.com/wp-content/uploads/2019/08/no-photo-icon-22.png"
+        />
+      </div>
     </main>
   );
 };
