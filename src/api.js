b3f3abaf9b8028ae3f7c55318fe8b581acf8ee14
mourad <mourad.latoui.89@gmail.com>
2023-07-05 22:10:33 -0400

    class 9 challenge 2

--- src/api.js
+++ src/api.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
-const { Profiles } = require('./Profiles');
+const Profiles = require('./Profiles');
 
 app.get('/api/profiles', (req, res) => {
   res.json(Profiles);
+});
+
+app.get('/api/profiles/:id', (req, res) => {
+  const id = req.params.id;
+  const profile = Profiles.find((profile) => profile.id === parseInt(id));
+
+  if (profile) {
+    res.json(profile);
+  } else {
+    res.status(404).json({ message: 'Profile not found' });
+  }
 });
 
 app.listen(3001, () => {
